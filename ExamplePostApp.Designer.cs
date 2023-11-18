84f08667edd3a04d8e3a1bfc15a0f0be8062347e
Seth Bailey <sebailey@deloitte.com>
2023-11-17 23:02:50 -0500

    Commit of working first pass at example .NET Framework WinForms app with peripheral (2D barcode scanner) functionality

---
+++ ExamplePostApp.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace fakeACSFramework
+{
+    partial class ExamplePostApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAppBanner1 = new System.Windows.Forms.Label();
+            this.lblAppBanner2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFormType = new System.Windows.Forms.Label();
+            this.lblFormTypeValue = new System.Windows.Forms.Label();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.txtPlaceOfBirth = new System.Windows.Forms.TextBox();
+            this.lblPlaceOfBirth = new System.Windows.Forms.Label();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.lblSSN = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.lblState = new System.Windows.Forms.Label();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.cmbSerialPorts = new System.Windows.Forms.ComboBox();
+            this.lblPortList = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAppBanner1
+            // 
+            this.lblAppBanner1.AutoSize = true;
+            this.lblAppBanner1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppBanner1.Location = new System.Drawing.Point(375, 9);
+            this.lblAppBanner1.Name = "lblAppBanner1";
+            this.lblAppBanner1.Size = new System.Drawing.Size(468, 42);
+            this.lblAppBanner1.TabIndex = 0;
+            this.lblAppBanner1.Text = "Example POST Application";
+            // 
+            // lblAppBanner2
+            // 
+            this.lblAppBanner2.AutoSize = true;
+            this.lblAppBanner2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppBanner2.Location = new System.Drawing.Point(377, 68);
+            this.lblAppBanner2.Name = "lblAppBanner2";
+            this.lblAppBanner2.Size = new System.Drawing.Size(477, 25);
+            this.lblAppBanner2.TabIndex = 1;
+            this.lblAppBanner2.Text = "Welcome to POST <get post name env variable>";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(64, 170);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(220, 20);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(61, 154);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(541, 170);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(286, 20);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(538, 154);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblFormType
+            // 
+            this.lblFormType.AutoSize = true;
+            this.lblFormType.Location = new System.Drawing.Point(61, 116);
+            this.lblFormType.Name = "lblFormType";
+            this.lblFormType.Size = new System.Drawing.Size(101, 13);
+            this.lblFormType.TabIndex = 6;
+            this.lblFormType.Text = "Passport Form Type";
+            // 
+            // lblFormTypeValue
+            // 
+            this.lblFormTypeValue.AutoSize = true;
+            this.lblFormTypeValue.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblFormTypeValue.Location = new System.Drawing.Point(168, 116);
+            this.lblFormTypeValue.Name = "lblFormTypeValue";
+            this.lblFormTypeValue.Size = new System.Drawing.Size(0, 13);
+            this.lblFormTypeValue.TabIndex = 7;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(318, 170);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(174, 20);
+            this.txtMiddleName.TabIndex = 8;
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Location = new System.Drawing.Point(321, 155);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(69, 13);
+            this.lblMiddleName.TabIndex = 9;
+            this.lblMiddleName.Text = "Middle Name";
+            // 
+            // txtSex
+            // 
+            this.txtSex.Location = new System.Drawing.Point(858, 170);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.Size = new System.Drawing.Size(98, 20);
+            this.txtSex.TabIndex = 10;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(860, 153);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(25, 13);
+            this.lblSex.TabIndex = 11;
+            this.lblSex.Text = "Sex";
+            // 
+            // txtPlaceOfBirth
+            // 
+            this.txtPlaceOfBirth.Location = new System.Drawing.Point(64, 236);
+            this.txtPlaceOfBirth.Name = "txtPlaceOfBirth";
+            this.txtPlaceOfBirth.Size = new System.Drawing.Size(252, 20);
+            this.txtPlaceOfBirth.TabIndex = 12;
+            // 
+            // lblPlaceOfBirth
+            // 
+            this.lblPlaceOfBirth.AutoSize = true;
+            this.lblPlaceOfBirth.Location = new System.Drawing.Point(61, 220);
+            this.lblPlaceOfBirth.Name = "lblPlaceOfBirth";
+            this.lblPlaceOfBirth.Size = new System.Drawing.Size(70, 13);
+            this.lblPlaceOfBirth.TabIndex = 13;
+            this.lblPlaceOfBirth.Text = "Place of Birth";
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Location = new System.Drawing.Point(361, 236);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(119, 20);
+            this.txtDOB.TabIndex = 14;
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Location = new System.Drawing.Point(358, 220);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(66, 13);
+            this.lblDateOfBirth.TabIndex = 15;
+            this.lblDateOfBirth.Text = "Date of Birth";
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Location = new System.Drawing.Point(523, 236);
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(233, 20);
+            this.txtSSN.TabIndex = 16;
+            // 
+            // lblSSN
+            // 
+            this.lblSSN.AutoSize = true;
+            this.lblSSN.Location = new System.Drawing.Point(520, 217);
+            this.lblSSN.Name = "lblSSN";
+            this.lblSSN.Size = new System.Drawing.Size(117, 13);
+            this.lblSSN.TabIndex = 17;
+            this.lblSSN.Text = "Social Security Number";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(64, 293);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(236, 20);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(63, 277);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 19;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(341, 294);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(200, 20);
+            this.txtCity.TabIndex = 20;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(338, 277);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 21;
+            this.lblCity.Text = "City";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(575, 294);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(61, 20);
+            this.txtState.TabIndex = 22;
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(572, 277);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 23;
+            this.lblState.Text = "State";
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(677, 293);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(79, 20);
+            this.txtZipCode.TabIndex = 24;
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.AutoSize = true;
+            this.lblZipCode.Location = new System.Drawing.Point(677, 278);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(50, 13);
+            this.lblZipCode.TabIndex = 25;
+            this.lblZipCode.Text = "Zip Code";
+            // 
+            // cmbSerialPorts
+            // 
+            this.cmbSerialPorts.FormattingEnabled = true;
+            this.cmbSerialPorts.Location = new System.Drawing.Point(66, 30);
+            this.cmbSerialPorts.Name = "cmbSerialPorts";
+            this.cmbSerialPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbSerialPorts.TabIndex = 26;
+            this.cmbSerialPorts.SelectionChangeCommitted += new System.EventHandler(this.cmbSerialPorts_SelectionChangeCommitted);
+            // 
+            // lblPortList
+            // 
+            this.lblPortList.AutoSize = true;
+            this.lblPortList.Location = new System.Drawing.Point(63, 14);
+            this.lblPortList.Name = "lblPortList";
+            this.lblPortList.Size = new System.Drawing.Size(108, 13);
+            this.lblPortList.TabIndex = 27;
+            this.lblPortList.Text = "Choose Scanner Port";
+            // 
+            // ExamplePostApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 552);
+            this.Controls.Add(this.lblPortList);
+            this.Controls.Add(this.cmbSerialPorts);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblSSN);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.lblDateOfBirth);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.lblPlaceOfBirth);
+            this.Controls.Add(this.txtPlaceOfBirth);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.txtSex);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.lblFormTypeValue);
+            this.Controls.Add(this.lblFormType);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblAppBanner2);
+            this.Controls.Add(this.lblAppBanner1);
+            this.Name = "ExamplePostApp";
+            this.Text = "Example POST application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAppBanner1;
+        private System.Windows.Forms.Label lblAppBanner2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFormType;
+        private System.Windows.Forms.Label lblFormTypeValue;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.TextBox txtPlaceOfBirth;
+        private System.Windows.Forms.Label lblPlaceOfBirth;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.Label lblSSN;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.ComboBox cmbSerialPorts;
+        private System.Windows.Forms.Label lblPortList;
+    }
+}
+
---
+++ ExamplePostApp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fakeACSFramework
+{
+    public partial class ExamplePostApp : Form
+    {
+        public SerialPort mySerialPort = new SerialPort();
+        public ExamplePostApp()
+        {
+            InitializeComponent();
+            String[] serialports = SerialPort.GetPortNames();
+
+            if (serialports.Length ==  0 )
+            {
+                MessageBox.Show("No COM ports for scanner found!");
+            }
+            else
+            {
+                foreach (string p in serialports)
+                {
+                    cmbSerialPorts.Items.Add(p);
+                }
+                cmbSerialPorts.SelectedIndex = 0;
+                SetupSerialPort();
+
+            }
+
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+
+
+        }
+               
+                
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            
+            string[] BarcodeValues = indata.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            lblFormTypeValue.Invoke((MethodInvoker)delegate {lblFormTypeValue.Text = BarcodeValues[0];});
+            txtFirstName.Invoke((MethodInvoker)delegate { txtFirstName.Text = BarcodeValues[2]; });
+            txtMiddleName.Invoke((MethodInvoker)delegate { txtMiddleName.Text = BarcodeValues[3]; });
+            txtLastName.Invoke((MethodInvoker)delegate { txtLastName.Text = BarcodeValues[4]; });
+            txtSex.Invoke((MethodInvoker)delegate { txtSex.Text = BarcodeValues[5]; });
+            txtPlaceOfBirth.Invoke((MethodInvoker)delegate { txtPlaceOfBirth.Text = BarcodeValues[6]; });
+            txtDOB.Invoke((MethodInvoker)delegate { txtDOB.Text = BarcodeValues[7]; });
+            txtSSN.Invoke((MethodInvoker)delegate { txtSSN.Text = BarcodeValues[8]; });
+            txtAddress.Invoke((MethodInvoker)delegate { txtAddress.Text = BarcodeValues[9]; });
+            txtCity.Invoke((MethodInvoker)delegate { txtCity.Text = BarcodeValues[10]; });
+            txtState.Invoke((MethodInvoker)delegate { txtState.Text = BarcodeValues[11]; });
+            txtZipCode.Invoke((MethodInvoker)delegate { txtZipCode.Text = BarcodeValues[12]; });
+            //File.WriteAllText("C:\\Users\\Seth\\Desktop\\barcoderead.txt", indata);
+            //MessageBox.Show("All done!");
+        }
+
+        private void cmbSerialPorts_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            SetupSerialPort();
+        }
+
+        private void SetupSerialPort()
+        {
+            if (mySerialPort.IsOpen)
+            {
+                mySerialPort.Close();
+            }
+
+            mySerialPort.PortName = cmbSerialPorts.SelectedItem.ToString();
+            mySerialPort.Open();
+        }
+    }
+
+}
